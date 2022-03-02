580be014ea0e02bb6b05f044569e6e3158fa65a1
K4R1M <karim.bentaous@gmail.com>
2022-03-02 21:49:06 +1100

    Update dice array to objects instead

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ export default function App() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
       let die = Math.floor(Math.random() * 6) + 1;
-      newDice.push(die)
+      newDice.push({ value: die, isHeld: false })
     }
     return newDice
   }
   const [dice, setDice] = useState(allNewDice())
+  console.log(dice)
 
-  const diceElements = dice.map(die => <Die value={die} />)
+  const diceElements = dice.map(die => <Die value={die.value} isHeld={die.isHeld} />)
 
   return (
     <main>
       <section className="dice-container" >
         {diceElements}
-        {console.log("rendered")}
+        {console.log(diceElements)}
       </section>
       <button
         className='btn-roll'
         onClick={() => setDice(allNewDice())} >Roll</button>
     </main>
   );
-}
+} 
