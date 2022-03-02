61987f0cb0c7888e7da4c424fa035187ff59bf6f
K4R1M <karim.bentaous@gmail.com>
2022-03-02 22:03:13 +1100

    - add nanoid package for UID

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Die from './components/Die';
+import { nanoid } from 'nanoid';
 
 export default function App() {
 
@@ -8,20 +9,25 @@ export default function App() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
       let die = Math.floor(Math.random() * 6) + 1;
-      newDice.push({ value: die, isHeld: false })
+      newDice.push({
+        id: nanoid(),
+        value: die,
+        isHeld: false
+      })
     }
     return newDice
   }
   const [dice, setDice] = useState(allNewDice())
-  console.log(dice)
 
-  const diceElements = dice.map(die => <Die value={die.value} isHeld={die.isHeld} />)
+  const diceElements = dice.map(die => <Die
+    key={die.id}
+    value={die.value}
+    isHeld={die.isHeld} />)
 
   return (
     <main>
       <section className="dice-container" >
         {diceElements}
-        {console.log(diceElements)}
       </section>
       <button
         className='btn-roll'
